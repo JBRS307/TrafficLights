94f11f3bd19f1557c57ebf58b52f9c375d0bb984
JBRS307 <kuba.rekas.307@gmail.com>
2024-11-19 23:27:40 +0100

    Added priority multiplier to configuration

--- traffic/src/main/java/pl/jbrs/traffic/simulation/Configuration.java
+++ traffic/src/main/java/pl/jbrs/traffic/simulation/Configuration.java
@@ -1,16 +1,35 @@
 package pl.jbrs.traffic.simulation;
 
 public class Configuration {
-
-    private final int stateLength;
-
-    public Configuration(int stateLength) {
-        this.stateLength = stateLength;
-    }
+    private int stateLength;
+    private int priorityMultiplier;
 
     // Empty constructor containing default values
     public Configuration() {
         this.stateLength = DefaultConfiguration.STATE_LENGTH;
+        this.priorityMultiplier = DefaultConfiguration.PRIORITY_MULTIPLIER;
+    }
+
+    public int getStateLength() {
+        return stateLength;
+    }
+
+    public void setStateLength(int stateLength) {
+        if (stateLength < 1) {
+            throw new IllegalArgumentException("stateLength must be greater than 0");
+        }
+        this.stateLength = stateLength;
+    }
+
+    public int getPriorityMultiplier() {
+        return priorityMultiplier;
+    }
+
+    public void setPriorityMultiplier(int priorityMultiplier) {
+        if (priorityMultiplier < 1) {
+            throw new IllegalArgumentException("priorityMultiplier must be greater than 0");
+        }
+        this.priorityMultiplier = priorityMultiplier;
     }
 
 }
--- traffic/src/main/java/pl/jbrs/traffic/simulation/DefaultConfiguration.java
+++ traffic/src/main/java/pl/jbrs/traffic/simulation/DefaultConfiguration.java
@@ -1,5 +1,10 @@
 package pl.jbrs.traffic.simulation;
 
 public class DefaultConfiguration {
+    // Base value of determining how many simulation steps should strategy state take
+    // It is adjusted by strategy
     public static final int STATE_LENGTH = 30;
+
+    // Determines how many cars is worth 1 priority point
+    public static final int PRIORITY_MULTIPLIER = 10;
 }
