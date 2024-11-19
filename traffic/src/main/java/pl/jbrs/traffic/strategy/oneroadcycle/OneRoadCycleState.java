a8ea38f1835b8a8a11f171f47d19d00c5e009b47
JBRS307 <kuba.rekas.307@gmail.com>
2024-11-19 23:28:31 +0100

    Added state enum for one road cycle strategy

---
+++ traffic/src/main/java/pl/jbrs/traffic/strategy/oneroadcycle/OneRoadCycleState.java
@@ -0,0 +1,7 @@
+package pl.jbrs.traffic.strategy.oneroadcycle;
+
+// This one is very basic, one whole direction has green
+// everyone else has red
+public enum OneRoadCycleState {
+    NORTH, EAST, SOUTH, WEST;
+}
