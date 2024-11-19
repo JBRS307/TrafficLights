6ac4c3e0408b55f831b0d3e00fecdf775e8cd8e2
JBRS307 <kuba.rekas.307@gmail.com>
2024-11-19 16:13:52 +0100

    Added function to calculate possible LaneDirection from MoveDirection in LaneDirection class

--- traffic/src/main/java/pl/jbrs/traffic/model/LaneDirection.java
+++ traffic/src/main/java/pl/jbrs/traffic/model/LaneDirection.java
@@ -1,5 +1,7 @@
 package pl.jbrs.traffic.model;
 
+import java.util.List;
+
 public enum LaneDirection {
     UTURN,
     UTURN_LEFT,
@@ -9,7 +11,7 @@ public enum LaneDirection {
     STRAIGHT_RIGHT,
     RIGHT;
 
-    public LaneDirection fromString(String direction) {
+    public static LaneDirection fromString(String direction) {
         return switch (direction) {
             case "uturn" -> UTURN;
             case "uturnLeft" -> UTURN_LEFT;
@@ -19,6 +21,20 @@ public enum LaneDirection {
             case "right" -> RIGHT;
             case "straightRight" -> STRAIGHT_RIGHT;
             default -> null;
+        };
+    }
+
+    // Function gets possible lane direction for given move direction
+    // That is for MoveDirection STRAIGHT it will return List.of(STRAIGHT_RIGHT, STRAIGHT, STRAIGHT_LEFT)
+    // List will be ordered from rightmost to leftmost direction
+    // In case of UTURN List.of(UTURN, UTURN_LEFT) is returned, cecause there can be only ONE uturn line
+    // which is either solely for uturn or for uturn and left turn
+    public static List<LaneDirection> fromMoveDirection(MoveDirection moveDirection) {
+        return switch (moveDirection) {
+            case UTURN -> List.of(UTURN, UTURN_LEFT);
+            case LEFT -> List.of(STRAIGHT_LEFT, LEFT);
+            case STRAIGHT -> List.of(STRAIGHT_LEFT, STRAIGHT, STRAIGHT_RIGHT);
+            case RIGHT -> List.of(STRAIGHT_RIGHT, STRAIGHT);
         };
     }
 }
