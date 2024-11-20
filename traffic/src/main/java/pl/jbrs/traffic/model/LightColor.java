edc32e7b141e899a55af9a364b64ccfb515e8432
JBRS307 <kuba.rekas.307@gmail.com>
2024-11-20 20:00:57 +0100

    Added comment

--- traffic/src/main/java/pl/jbrs/traffic/model/LightColor.java
+++ traffic/src/main/java/pl/jbrs/traffic/model/LightColor.java
@@ -4,7 +4,7 @@ public enum LightColor {
     GREEN,
     YELLOW,
     RED,
-    RED_YELLOW;
+    RED_YELLOW; // Even tough present, this state is omitted, because it doesn't add any value to the simulation and increases complexity
     // In Poland when green light is about to light up the red and yellow lights are turned on at the same time.
     // When green is about to change to res yellow light is turned on alone.
 
