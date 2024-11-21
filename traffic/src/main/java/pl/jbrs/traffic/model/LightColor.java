b8610edfd5bdd92f48ee323cd002982afbaa3a8e
JBRS307 <kuba.rekas.307@gmail.com>
2024-11-21 21:59:30 +0100

    Removed methods from LightColor enum

--- traffic/src/main/java/pl/jbrs/traffic/model/LightColor.java
+++ traffic/src/main/java/pl/jbrs/traffic/model/LightColor.java
@@ -7,21 +7,4 @@ public enum LightColor {
     RED_YELLOW; // Even tough present, this state is omitted, because it doesn't add any value to the simulation and increases complexity
     // In Poland when green light is about to light up the red and yellow lights are turned on at the same time.
     // When green is about to change to res yellow light is turned on alone.
-
-    public LightColor next() {
-        return switch (this) {
-            case RED -> RED_YELLOW;
-            case RED_YELLOW -> GREEN;
-            case GREEN -> YELLOW;
-            case YELLOW -> RED;
-        };
-    }
-
-    public LightColor nextPedestrian() {
-        return switch (this) {
-            case RED -> GREEN;
-            case GREEN -> RED;
-            default -> throw new IllegalStateException("Unexpected value for pedestrian light: " + this);
-        };
-    }
 }
