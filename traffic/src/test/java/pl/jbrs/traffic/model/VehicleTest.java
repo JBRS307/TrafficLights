225af1730887e18be0fbcf1fb55b47e29b99c7e1
JBRS307 <kuba.rekas.307@gmail.com>
2024-11-23 20:20:58 +0100

    Vehicle test done

---
+++ traffic/src/test/java/pl/jbrs/traffic/model/VehicleTest.java
@@ -0,0 +1,34 @@
+package pl.jbrs.traffic.model;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import pl.jbrs.traffic.model.road.PedestrianRoad;
+import pl.jbrs.traffic.model.road.Road;
+import pl.jbrs.traffic.model.road.RoadDirection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VehicleTest {
+    @Test
+    public void canDriveTest() {
+        // given
+        Map<RoadDirection, Road> greenRoads = new HashMap<>();
+        PedestrianRoad pedestrianRoad = new PedestrianRoad(RoadDirection.EAST, Map.of(), new PedestrianLight(), 0);
+        greenRoads.put(RoadDirection.EAST, pedestrianRoad);
+
+        Vehicle vehicle = new Vehicle("v1", RoadDirection.SOUTH, RoadDirection.EAST);
+
+        // when, then
+        assertFalse(vehicle.canDrive(greenRoads));
+
+        // given
+        vehicle = new Vehicle("v2", RoadDirection.SOUTH, RoadDirection.NORTH);
+        // when, then
+        assertTrue(vehicle.canDrive(greenRoads));
+
+    }
+
+}
