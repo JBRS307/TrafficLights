6fb3ee186aa44fff1330905bbe236a56d81cc8c1
JBRS307 <kuba.rekas.307@gmail.com>
2024-11-24 13:00:16 +0100

    Added JSONWriter

---
+++ traffic/src/main/java/pl/jbrs/traffic/json/writer/JSONWriter.java
@@ -0,0 +1,19 @@
+package pl.jbrs.traffic.json.writer;
+
+import org.json.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class JSONWriter {
+    public static void writeJSON(String path, JSONObject json) throws IOException {
+        String jsonString = json.toString(4);
+        try {
+            FileWriter fileWriter = new FileWriter(path);
+            fileWriter.write(jsonString);
+            fileWriter.close();
+        } catch (IOException e) {
+            System.err.println("Error writing to file: " + e.getMessage());
+        }
+    }
+}
